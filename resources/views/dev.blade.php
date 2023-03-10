92dfc25a54f01cf8d45861b9bec92967b2c1dabb
Dodo armando <dodoamrnando30@gmail.com>
2023-03-10 23:51:58 +0700

    Add Alpine JS

--- resources/views/dev.blade.php
+++ resources/views/dev.blade.php
@@ -1,86 +1,124 @@
-<!doctype html>
-<html>
+<!DOCTYPE html>
+<html lang="en">
 
 <head>
-    <meta charset="utf-8">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Website Resmi Desa Sidomukti</title>
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body>
-    <nav class="bg-gray-900 text-white">
-        <div class="container mx-auto px-4">
-            <div class="flex items-center justify-between py-4">
-                <a href="#" class="text-2xl font-bold">Blog</a>
-                <button class="block md:hidden text-white focus:outline-none">
-                    <svg class="h-6 w-6 fill-current" viewBox="0 0 24 24">
-                        <path fill-rule="evenodd" clip-rule="evenodd" d="M4 6h16v2H4V6zm0 5h16v2H4v-2zm0 5h16v2H4v-2z">
-                        </path>
-                    </svg>
-                </button>
-                <div class="hidden md:block">
-                    <ul class="flex">
-                        <li class="mr-6"><a href="#" class="hover:text-gray-400">Home</a></li>
-                        <li class="mr-6"><a href="#" class="hover:text-gray-400">About</a></li>
-                    </ul>
-                </div>
+<body x-data="{ navOpen: false, scrolledFromTop: false }" x-init="window.pageYOffset >= 50 ? scrolledFromTop = true : scrolledFromTop = false"
+    @scroll.window="window.pageYOffset >= 50 ? scrolledFromTop = true : scrolledFromTop = false"
+    :class="{
+        'overflow-hidden': navOpen,
+        'overflow-scroll': !navOpen
+    }">
+    <header
+        class="
+            fixed
+            w-full
+            bg-primary
+            flex
+            justify-between
+            items-center
+            px-4
+            md:px-12
+            transition-all
+            duration-200
+            h-24
+        "
+        :class="{ 'h-24': !scrolledFromTop, 'h-14': scrolledFromTop }">
+        <a href="#">
+            <img src="./assets/images/logo.svg" alt="ChitChat Logo"
+                class="h-12 transform origin-left transition duration-200"
+                :class="{ 'scale-100': !scrolledFromTop, 'scale-75': scrolledFromTop }" />
+        </a>
+        <nav>
+            <button class="md:hidden" @click="navOpen = !navOpen">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 text-white" fill="none" viewBox="0 0 24 24"
+                    stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                </svg>
+            </button>
+            <ul class="
+                  fixed
+                  left-0
+                  right-0
+                  min-h-screen
+                  px-4
+                  pt-8
+                  space-y-4
+                  text-white
+                  bg-primary
+                  transform
+                  transition
+                  duration-300
+                  translate-x-full
+                  md:relative md:flex md:space-x-10 md:min-h-0 md:px-0 md:py-0 md:space-y-0 md:translate-x-0
+               "
+                :class="{ 'translate-x-full': !navOpen }" :class="{ 'translate-x-0': navOpen }">
+                <li class="">
+                    <a href="#" @click="navOpen = false"
+                        class="font-black
+                        tracking-wide
+                        uppercase
+                        ">
+                        Profil Desa
+                    </a>
+                </li>
+                <li class="">
+                    <a href="#" @click="navOpen = false"
+                        class="font-black
+                        tracking-wide
+                        uppercase
+                        ">
+                        Pemerintahan
+                    </a>
+                </li>
+                <li class="">
+                    <a href="#" @click="navOpen = false"
+                        class="font-black
+                        tracking-wide
+                        uppercase
+                        ">
+                        Informasi
+                    </a>
+                </li>
+                <li class="">
+                    <a href="#" @click="navOpen = false"
+                        class="font-black
+                        tracking-wide
+                        uppercase
+                        ">
+                        Transparasi
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+    <section class="pt-32 pb-16 px-8 md:px-12 bg-blue-500">
+        <div class="max-w-7xl mx-auto md:flex md:items-center md:justify-between">
+            <div class="md:flex-1 md:mr-6">
+                <h1 class="font-bold text-4xl md:text-5xl text-white leading-tight">
+                    The Quickest way to Chat with your Loved Ones
+                </h1>
+                <p class="mt-4 text-lg text-white">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis aspernatur magni vitae
+                    veritatis.
+                </p>
             </div>
-        </div>
-    </nav>
-
-    <section class="bg-gray-900 text-white">
-        <div class="container mx-auto px-4 py-16">
-            <div class="md:flex md:items-center md:justify-between">
-                <div class="md:w-1/2">
-                    <h1 class="text-4xl font-bold mb-4">Welcome to our blog!</h1>
-                    <p class="text-xl mb-8">Stay up-to-date with the latest news and trends.</p>
-                    <a href="#"
-                        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">Read Our
-                        Blog</a>
-                </div>
-                <div class="md:w-1/2">
-                    <img src="https://via.placeholder.com/640x480.png" alt=""
-                        class="w-full h-64 object-cover rounded-lg shadow-lg">
-                </div>
+            <div class="md:flex-1">
+                <img src="https://res.cloudinary.com/thirus/image/upload/v1632162912/logos/chat_ys7mog.svg"
+                    alt="Chat with loved ones" />
             </div>
         </div>
     </section>
-
-    <div class="container mx-auto px-4">
-        <h1 class="text-2xl font-bold my-8">Latest News</h1>
-        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-            <div class="bg-white shadow rounded-lg overflow-hidden">
-                <img src="https://via.placeholder.com/640x480.png" alt="" class="w-full h-64 object-cover">
-                <div class="p-4">
-                    <h2 class="text-lg font-bold mb-2"><a href="#">Blog Post Title 1</a></h2>
-                    <p class="text-gray-700 mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                        eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                    <a href="#" class="text-blue-500 hover:text-blue-700">Read more</a>
-                </div>
-            </div>
-            <div class="bg-white shadow rounded-lg overflow-hidden">
-                <img src="https://via.placeholder.com/640x480.png" alt="" class="w-full h-64 object-cover">
-                <div class="p-4">
-                    <h2 class="text-lg font-bold mb-2"><a href="#">Blog Post Title 2</a></h2>
-                    <p class="text-gray-700 mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                        eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                    <a href="#" class="text-blue-500 hover:text-blue-700">Read more</a>
-                </div>
-            </div>
-            <div class="bg-white shadow rounded-lg overflow-hidden">
-                <img src="https://via.placeholder.com/640x480.png" alt="" class="w-full h-64 object-cover">
-                <div class="p-4">
-                    <h2 class="text-lg font-bold mb-2"><a href="#">Blog Post Title 3</a></h2>
-                    <p class="text-gray-700 mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                        eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                    <a href="#" class="text-blue-500 hover:text-blue-700">Read more</a>
-                </div>
-            </div>
-        </div>
-    </div>
-
-
-
+    <section id="features" class="min-h-screen"></section>
+    <section id="about" class="min-h-screen bg-gray-100"></section>
+    <section id="contact" class="min-h-screen"></section>
 </body>
 
 </html>
