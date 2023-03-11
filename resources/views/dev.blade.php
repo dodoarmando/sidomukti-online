54d7adc9cf6e34112495d5a91e745e6b555f4e20
Dodo armando <dodoamrnando30@gmail.com>
2023-03-11 22:08:48 +0700

    Remove Old CSS And Use Tailwind

--- resources/views/dev.blade.php
+++ resources/views/dev.blade.php
@@ -6,119 +6,49 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Website Resmi Desa Sidomukti</title>
-    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body x-data="{ navOpen: false, scrolledFromTop: false }" x-init="window.pageYOffset >= 50 ? scrolledFromTop = true : scrolledFromTop = false"
-    @scroll.window="window.pageYOffset >= 50 ? scrolledFromTop = true : scrolledFromTop = false"
-    :class="{
-        'overflow-hidden': navOpen,
-        'overflow-scroll': !navOpen
-    }">
-    <header
-        class="
-            fixed
-            w-full
-            bg-primary
-            flex
-            justify-between
-            items-center
-            px-4
-            md:px-12
-            transition-all
-            duration-200
-            h-24
-        "
-        :class="{ 'h-24': !scrolledFromTop, 'h-14': scrolledFromTop }">
-        <a href="#">
-            <img src="./assets/images/logo.svg" alt="ChitChat Logo"
-                class="h-12 transform origin-left transition duration-200"
-                :class="{ 'scale-100': !scrolledFromTop, 'scale-75': scrolledFromTop }" />
-        </a>
-        <nav>
-            <button class="md:hidden" @click="navOpen = !navOpen">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 text-white" fill="none" viewBox="0 0 24 24"
-                    stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                </svg>
-            </button>
-            <ul class="
-                  fixed
-                  left-0
-                  right-0
-                  min-h-screen
-                  px-4
-                  pt-8
-                  space-y-4
-                  text-white
-                  bg-primary
-                  transform
-                  transition
-                  duration-300
-                  translate-x-full
-                  md:relative md:flex md:space-x-10 md:min-h-0 md:px-0 md:py-0 md:space-y-0 md:translate-x-0
-               "
-                :class="{ 'translate-x-full': !navOpen }" :class="{ 'translate-x-0': navOpen }">
-                <li class="">
-                    <a href="#" @click="navOpen = false"
-                        class="font-black
-                        tracking-wide
-                        uppercase
-                        ">
-                        Profil Desa
-                    </a>
-                </li>
-                <li class="">
-                    <a href="#" @click="navOpen = false"
-                        class="font-black
-                        tracking-wide
-                        uppercase
-                        ">
-                        Pemerintahan
-                    </a>
-                </li>
-                <li class="">
-                    <a href="#" @click="navOpen = false"
-                        class="font-black
-                        tracking-wide
-                        uppercase
-                        ">
-                        Informasi
-                    </a>
-                </li>
-                <li class="">
-                    <a href="#" @click="navOpen = false"
-                        class="font-black
-                        tracking-wide
-                        uppercase
-                        ">
-                        Transparasi
-                    </a>
-                </li>
-            </ul>
-        </nav>
-    </header>
-    <section class="pt-32 pb-16 px-8 md:px-12 bg-blue-500">
-        <div class="max-w-7xl mx-auto md:flex md:items-center md:justify-between">
-            <div class="md:flex-1 md:mr-6">
-                <h1 class="font-bold text-4xl md:text-5xl text-white leading-tight">
-                    The Quickest way to Chat with your Loved Ones
-                </h1>
-                <p class="mt-4 text-lg text-white">
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis aspernatur magni vitae
-                    veritatis.
-                </p>
-            </div>
-            <div class="md:flex-1">
-                <img src="https://res.cloudinary.com/thirus/image/upload/v1632162912/logos/chat_ys7mog.svg"
-                    alt="Chat with loved ones" />
+<body>
+    {{-- Header Start --}}
+    <header class="absolute top-0 left-0 z-10 flex w-full items-center bg-transparent">
+        <div class="container">
+            <div class="relative flex items-center justify-between bg-slate-500">
+                <div class="px-4">
+                    <a href="#home" class="block py-6 text-lg font-bold text-primary">Desa Sidomukti</a>
+                </div>
+                <div class="flex items-center px-4">
+                    <button id="hamburger" name="hamburger" type="button" class="absolute right-4 block lg:hidden">
+                        <span class="hamburger-line origin-top-left transition duration-300 ease-in-out"></span>
+                        <span class="hamburger-line transition duration-300 ease-in-out"></span>
+                        <span class="hamburger-line origin-bottom-left transition duration-300 ease-in-out"></span>
+                    </button>
+                    <nav id="nav-menu"
+                        class="hidden absolute py-5 bg-white shadow-lg rounded-lg max-w-[250px] w-full right-4 top-full lg:block lg:static lg:bg-transparent lg:max-w-full lg:shadow-none lg:rounded-none">
+                        <ul class="block lg:flex">
+                            <li class="group">
+                                <a href="#home"
+                                    class="text-base text-dark py-2 mx-8 flex group-hover:text-primary">Beranda</a>
+                            </li>
+                            <li class="group">
+                                <a href="#home"
+                                    class="text-base text-dark py-2 mx-8 flex group-hover:text-primary">Beranda</a>
+                            </li>
+                            <li class="group">
+                                <a href="#home"
+                                    class="text-base text-dark py-2 mx-8 flex group-hover:text-primary">Beranda</a>
+                            </li>
+                            <li class="group">
+                                <a href="#home"
+                                    class="text-base text-dark py-2 mx-8 flex group-hover:text-primary">Beranda</a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
             </div>
         </div>
-    </section>
-    <section id="features" class="min-h-screen"></section>
-    <section id="about" class="min-h-screen bg-gray-100"></section>
-    <section id="contact" class="min-h-screen"></section>
+    </header>
+    {{-- Header End --}}
 </body>
 
 </html>
--- resources/views/layouts/blog.blade.php
+++ resources/views/layouts/blog.blade.php
@@ -6,16 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Sidomukti</title>
-    <!-- plugin css for this page -->
-    <link rel="stylesheet" href="./assets/vendors/mdi/css/materialdesignicons.min.css" />
-    <link rel="stylesheet" href="assets/vendors/aos/dist/aos.css/aos.css" />
-
-    <!-- End plugin css for this page -->
-    <link rel="shortcut icon" href="assets/images/favicon.png" />
-
-    <!-- inject:css -->
-    <link rel="stylesheet" href="assets/css/style.css">
-    <!-- endinject -->
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body>
@@ -32,16 +24,6 @@
             @include('components.footer')
         </div>
     </div>
-    <!-- inject:js -->
-    <script src="assets/vendors/js/vendor.bundle.base.js"></script>
-    <!-- endinject -->
-    <!-- plugin js for this page -->
-    <script src="assets/vendors/aos/dist/aos.js/aos.js"></script>
-    <!-- End plugin js for this page -->
-    <!-- Custom js for this page-->
-    <script src="./assets/js/demo.js"></script>
-    <script src="./assets/js/jquery.easeScroll.js"></script>
-    <!-- End custom js for this page-->
 </body>
 
 </html>
